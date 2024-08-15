e6033d987b387c044c2526fa4ba4d52029601bbd
om50 <omchy34@gmail.com>
2024-08-15 17:01:45 +0530

    changes

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -15,19 +15,18 @@ const App = () => {
       <Router>
         <Routes>
           {/* Admin Login Route */}
-          <Route path="/" element={token ? <Navigate to={<CertificatesRequests/>} /> : <AdminLogin />} />
+          <Route path="/" element={token ? <Navigate to="/admin/certificates-requests" /> : <AdminLogin />} />
 
           {/* Protected Admin Routes */}
           {token && (
             <Route path="/admin" element={<AdminLayout />}>
-              <Route index element={<Navigate to="certificates-requests" />} /> {/* Default route */}
               <Route path="certificates-requests" element={<CertificatesRequests />} />
               <Route path="addworkshop" element={<AddWorkShop />} />
             </Route>
           )}
 
           {/* Redirect to login if no token */}
-          <Route path="*" element={token ? <Navigate to="/admin" replace /> : <Navigate to="/" replace />} />
+          {!token && <Route path="*" element={<Navigate to="/" replace />} />}
         </Routes>
         <ToastContainer />
       </Router>
