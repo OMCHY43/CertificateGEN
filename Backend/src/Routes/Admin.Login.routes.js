253c73814677ada0da765eb3938c61365edbf893
om50 <omchy34@gmail.com>
2024-08-15 09:59:10 +0530

    bug fix

--- Backend/src/Routes/Admin.Login.routes.js
+++ Backend/src/Routes/Admin.Login.routes.js
@@ -1,5 +1,5 @@
 import {Router} from "express"
-import { AdminLogin } from "../Controller/Admin.controller,js"
+import { AdminLogin } from "../Controller/Admin.controller.js"
 
 const router = Router()
 
