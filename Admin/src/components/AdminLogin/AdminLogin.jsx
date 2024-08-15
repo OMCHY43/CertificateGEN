37a4f3adc496b86f77e2e4eda60bc52cdfcfc175
om50 <omchy34@gmail.com>
2024-08-15 17:13:15 +0530

    changes

--- Admin/src/components/AdminLogin/AdminLogin.jsx
+++ Admin/src/components/AdminLogin/AdminLogin.jsx
@@ -28,7 +28,7 @@ const AdminLogin = () => {
         };
 
         checkAdminExists();
-    }, []);
+    }, [navigate]);
 
     const handleChange = (e) => {
         setFormData({ ...formData, [e.target.name]: e.target.value });
